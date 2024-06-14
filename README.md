65271086ffb0602513ab453ee91f82ca0924e5cf
Elliott Mangham <elliott@coderesolution.com>
2024-06-14 01:02:18 +0100

    Improve test code and add basic example to readme

--- README.md
+++ README.md
@@ -2,6 +2,34 @@
 
 Work in progress.
 
+## Example
+
+```js
+// Import utility
+import DomInject from '../src/index'
+
+// Create an instance of the DomInject class
+const domInject = new DomInject()
+
+// Test
+const trigger = document.querySelector('.trigger')
+
+domInject.loadContent(, 'page.html', '.content', {
+	mode: 'replace',
+	includeParent: true,
+	beforeFetch: (trigger) => console.log('Before fetch', trigger),
+	afterFetch: (trigger) => {
+		console.log('After fetch', trigger)
+		// Add animations here
+	},
+	onError: (error) => {
+		console.error('Custom error handler:', error)
+	},
+	muteErrors: true,
+})
+
+```
+
 ## License
 
 [The MIT License (MIT)](LICENSE)
--- docs/main.js
+++ docs/main.js
@@ -9,27 +9,42 @@ const domInject = new DomInject()
 
 // Test
 const triggers = document.querySelectorAll('[data-fetch]')
+const content = document.querySelector('.content')
 
 triggers.forEach(function (trigger) {
 	trigger.addEventListener('click', () => {
-		domInject.loadContent(trigger, '/sub/3.html', '.content', {
-			mode: 'replace',
-			includeParent: true,
+		const contentSource = trigger.dataset.fetchUrl || null
+		const sourceScope = trigger.dataset.fetchScope || null
+
+		domInject.loadContent(trigger, contentSource, sourceScope, {
+			mode: trigger.dataset.fetchMode ?? 'replace',
+			includeParent: trigger.dataset.includeParent ?? true,
 			beforeFetch: (trigger) => console.log('Before fetch', trigger),
 			afterFetch: (trigger) => {
 				console.log('After fetch', trigger)
-				// GSAP animation
-				gsap.from(trigger.querySelectorAll('p'), {
-					opacity: 0,
-					y: 20,
-					stagger: 0.1,
-					duration: 0.5,
-				})
+
+				if (!trigger.dataset.fetchSource && trigger.dataset.fetchScope) {
+					const fetchedContent = trigger.querySelectorAll(trigger.dataset.fetchScope)
+					fetchedContent.forEach(function (content) {
+						content.classList.remove('-hidden')
+					})
+				}
+
+				gsap.fromTo(
+					trigger.querySelectorAll('h1, p'),
+					{ opacity: 0, y: 20 },
+					{
+						opacity: 1,
+						y: 0,
+						stagger: 0.1,
+						duration: 0.5,
+					},
+				)
 			},
-			onError: (error) => {
-				console.error('Custom error handler:', error)
-			},
-			muteErrors: true,
+			// onError: (error) => {
+			// 	console.error('Custom error handler:', error)
+			// },
+			// muteErrors: true,
 		})
 	})
 })
