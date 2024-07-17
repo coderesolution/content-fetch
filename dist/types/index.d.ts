4b5ade840fa268ef7e1f0928fc3c197883125137
Elliott Mangham <elliott@coderesolution.com>
2024-07-17 12:28:40 +0100

    Recompile code

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,20 +1,32 @@
-/**
- * Written by Elliott Mangham at Code Resolution. Maintained by Code Resolution.
- * made@coderesolution.com
- */
-export default class DomInject {
+export default class ContentFetch {
     constructor(options?: {});
     options: {
         loadingClass: any;
         loadedClass: any;
         errorClass: any;
+        debugMode: any;
     };
-    fetchContent(sourceUrl: any, sourceScope?: any, includeParent?: boolean): Promise<any>;
-    loadContent(target: any, sourceUrl?: any, sourceScope?: any, { mode, beforeFetch, afterFetch, includeParent }?: {
-        mode?: string;
-        beforeFetch?: any;
-        afterFetch?: any;
+    cache: Map<any, any>;
+    controller: AbortController;
+    log(message: any): void;
+    fetchContent(url: any, sourceScope?: any, includeParent?: boolean): Promise<any>;
+    from({ selector, url, includeParent, onStart, onEnd, onError }: {
+        selector: any;
+        url?: string;
         includeParent?: boolean;
-    }): void;
-    _toggleLoadingState(element: any, isLoading: any, isError?: boolean): void;
+        onStart: any;
+        onEnd: any;
+        onError: any;
+    }): Promise<any>;
+    to({ destination, data, mode, delay, onStart, onEnd, onError }: {
+        destination: any;
+        data: any;
+        mode?: string;
+        delay?: number;
+        onStart: any;
+        onEnd: any;
+        onError: any;
+    }): Promise<any>;
+    fromTo(fromParams: any, toParams: any): void;
+    abortFetch(): void;
 }
