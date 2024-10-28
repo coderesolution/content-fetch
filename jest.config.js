9f819608efed788c4696bbdc8276d6ca54fb3150
Elliott Mangham <elliott@coderesolution.com>
2024-10-28 13:30:29 +0000

    Remove GI Action tests temporarily

--- jest.config.js
+++ jest.config.js
@@ -2,23 +2,7 @@ module.exports = {
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	verbose: true,
-	testTimeout: 10000,
-	reporters: [
-		'default',
-		[
-			'jest-junit',
-			{
-				outputDirectory: 'coverage',
-				outputName: 'junit.xml',
-				classNameTemplate: '{classname}',
-				titleTemplate: '{title}',
-				ancestorSeparator: ' › ',
-				usePathForSuiteName: true,
-			},
-		],
-	],
-	bail: 1,
-	detectOpenHandles: true,
-	errorOnDeprecated: true,
-	logHeapUsage: true,
+	transform: {
+		'^.+\\.js$': 'babel-jest',
+	},
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,22 +1,2 @@
 import 'whatwg-fetch'
 import '@testing-library/jest-dom'
-
-// Ensure window and document are available
-if (typeof window === 'undefined') {
-	global.window = {}
-}
-if (typeof document === 'undefined') {
-	global.document = {}
-}
-
-// Setup DOMParser
-if (typeof DOMParser === 'undefined') {
-	global.DOMParser = class DOMParser {
-		parseFromString(string, type) {
-			return {
-				body: { innerHTML: string },
-				querySelector: () => ({ innerHTML: string, outerHTML: string }),
-			}
-		}
-	}
-}
